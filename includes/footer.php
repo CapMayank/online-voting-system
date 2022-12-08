320ca659f66dedc71f59f2b95be1cf0a9bd44880
Mayank Vishwakarma <censedocean@gmail.com>
2022-12-08 08:50:59 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Online Voting System</b>
+      </div>
+      <strong> By<a href="https://www.linkedin.com/in/mayank-vishwakarma2004/"> Mayank Vishwakarma</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
